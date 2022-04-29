d5a42a9afe119bb3ed5ce65450310dce580c0e59
Ethan Page <ethanpage101@gmail.com>
2022-04-29 15:15:09 -0500

    updating queries

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ logged_in = False;
 
 import psycopg2
 from config import config
+from create_table import create_tables
 
 # Userchoice function that does basic input test with values being amount of choices
 def userChoice(values):
@@ -124,6 +125,34 @@ def login():
             username = input();
             print("Enter Password: ", end = '')
             password = input();
+
+            sql = """
+                    SELECT username 
+                    FROM   users
+                    WHERE  username = %(username)s
+                        AND users.password = %(password)s
+                    """
+            data = ()
+            conn = None
+            try:
+                # read database configuration
+                params = config()
+                # connect to the PostgreSQL database
+                conn = psycopg2.connect(**params)
+                # create a new cursor
+                cur = conn.cursor()
+                # execute the INSERT statement
+                cur.execute(sql, {'username': username, 'password': password})
+                # commit the changes to the database
+                conn.commit()
+                # close communication with the database
+                cur.close()
+            except (Exception, psycopg2.DatabaseError) as error:
+                print(error)
+            finally:
+                if conn is not None:
+                    conn.close()
+
             if ((username == fake_username) & (password == fake_password)):
                 logged_in = True
                 loggedInMenu()
@@ -158,6 +187,7 @@ def menu():
         
 
 def main():
+    create_tables()
     menu()
 
 if __name__ == "__main__":
