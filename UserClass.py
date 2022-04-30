7b53f0eb2869daa59de79d2bbe5d8e87412c57a2
Krunal Patel <krp356@msstate.edu>
2022-04-30 13:04:53 -0500

    Added in User Class Example
    User class example added

--- UserClass.py
+++ UserClass.py
@@ -1,8 +1,21 @@
 class User:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+  def __init__(self):
+    self.username = ""
+    self.firstname = ""
+    self.lastname = ""
+    self.password = ""
+    self.shippingaddress = ""
+    self.shippingstate = ""
+    self.shippingzip = ""
+    self.ccnum = ""
 
-  def myfunc(self):
-    print("Hello my name is " + self.name)
 
+  def createAccount(self, connection):
+    print("Test")
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM users")
+    result = cursor.fetchall()
+
+    for x in result:
+        print(x) ## all
+        
